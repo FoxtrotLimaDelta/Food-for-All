47aed669921385a27ecd98f57eac0fee560768f0
FoxtrotLimaDelta <61523717+FoxtrotLimaDelta@users.noreply.github.com>
2020-11-22 21:49:58 -0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # import necessary libraries
 import os
-import numpy as np
 from flask import (
     Flask,
     render_template,
@@ -11,9 +10,6 @@ from flask import (
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from sklearn.linear_model import LogisticRegression
-import pickle
-from sklearn.preprocessing import MinMaxScaler
 
 
 
@@ -26,7 +22,7 @@ app = Flask(__name__)
 # create route that renders index.html template
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("foodcalculator.html")
 # need routes for all html pages
 
 
@@ -39,7 +35,6 @@ def send():
         ohu2010 = request.form["OHU2010"]
         numgqtrs = request.form["NUMGQTRS"]
         familyincome = request.form["MedianFamilyIncome"]
-        tractKids = request.form["TractKids"]
         seniors = request.form["TractSeniors"]
         tractwhite = request.form["TractWhite"]
         tractblack = request.form["TractBlack"]
@@ -48,19 +43,15 @@ def send():
         trackaian = request.form["TractAIAN"]  
         multir = request.form["TractOMultir"]
         hispanic = request.form["TractHispanic"]
-        tractHUNV = request.form["TractHUNV"]
         snap = request.form["TractSNAP"]
-        distance = request.form["distance"]
         busstop = request.form["bus_stops"]
         storecount = request.form["store_count"]
              
 
 
-    input_data = np.array([ohu2010,
+    input_data = [ohu2010,
                   numgqtrs,
-                    familyincome,
-                    tractKids,
-                    seniors,
+                    familyincome,seniors,
                     tractwhite,
                     tractblack,
                     tractasian,
@@ -68,61 +59,14 @@ def send():
                     trackaian,  
                     multir,
                     hispanic,
-                    tractHUNV,
                     snap,
-                    distance,
                     busstop,
-                    storecount])
+                    storecount]
 
-    
-    X = input_data.reshape(1,-1)
-    print(X)
-    
-    X = scaler.transform(X)
-    
-    prediction = loaded_model.predict(X).tolist()
-    print (prediction)
-    
-    if prediction[0]== 1: 
-        prediction = "Food Desert"
-    else: 
-        prediction = "Not a Food Desert"
-    
-    results = {"prediction":prediction}
-    return render_template("foodcalculator.html", results=results)
 
-@app.route("/about.html")
-def about():
-       
-   return render_template("about.html")
 
-@app.route("/foodstory.html")
-def foodstory():
-       
-   return render_template("foodstory.html")
-
-@app.route("/geomap.html")
-def geomap():
-       
-   return render_template("geomap.html")
-
-@app.route("/foodcalculator.html")
-def foodcalculator():
-       
-   return render_template("foodcalculator.html")
-
-@app.route("/research.html")
-def research():
-       
-   return render_template("research.html")
-
-@app.route("/traditional.html")
-def traditional():
-       
-   return render_template("traditional.html")
+    return jsonify(input_data)
 
 
 if __name__ == "__main__":
-    scaler = pickle.load(open("x_scale.sav", 'rb'))
-    loaded_model = pickle.load(open("logistic_regression.sav", 'rb'))
     app.run()
---
+++ static/app.py
@@ -0,0 +1,94 @@
+6# import necessary libraries
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+#from config import username, password
+#engine = create_engine(f'postgresql://{username}:{password}@localhost:5433/petpals')
+
+engine = create_engine(os.environ.get('DATABASE_URL', ''))
+
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#Pet = Base.classes.pets
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def home():
+    return render_template("foodcalculator.html")
+
+# Query the database and send the jsonified results
+@app.route("/send", methods=["GET", "POST"])
+def send():
+
+    session = Session(engine)
+
+    if request.method == "GET":
+        ohu2010 = request.form["OHU2010"]
+        numgqtrs = request.form["NUMGQTRS"]
+        familyincome = request.form["MedianFamilyIncome"]
+        seniors = request.form["TractSeniors"]
+        tractwhite = request.form["TractWhite"]
+        tractblack = request.form["TractBlack"]
+        tractasian = request.form["TractAsian"]
+        tracthopi = request.form["TractNHOPI"]
+        trackaian = request.form["TractAIAN"]  
+        multir = request.form["TractOMultir"]
+        hispanic = request.form["TractHispanic"]
+        snap = request.form["TractSNAP"]
+        busstop = request.form["bus_stops"]
+        storecount = request.form["store_count"]
+             
+
+ pet = Pet(name=name, lat=lat, lon=lon)
+        session.add(pet)
+        session.commit()
+
+        session.close()
+
+
+        return redirect("/", code=302)
+loading up a value and passing in the prediction 
+    return render_template("foodcalculator.html", answer = predictedvalue)
+
+
+@app.route("/api/pals")
+def pals():
+
+    session = Session(engine)
+
+    results = session.query(Pet.name, Pet.lat, Pet.lon).all()
+
+    names = [result[0] for result in results]
+    lat = [result[1] for result in results]
+    lon = [result[2] for result in results]
+
+    pet_data = {
+        "latitude": lat,
+        "longitude": lon,
+        "hover_text": names
+    }
+
+    session.close()
+
+    return jsonify(pet_data)
+
+
+if __name__ == "__main__":
+    app.run()
